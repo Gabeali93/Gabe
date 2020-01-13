bea272495e01454f622d198a62c13c79d9e390da
Gabe Ali <gabeali93@gmail.com>
2020-01-13 15:27:10 -0500

    Initial commit

---
+++ C practice/performance15.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/* This program uses a for loop to print out a string */
+int main(){
+    //initializes array "Array" and gives value.
+    char Array [256][15] = {"stop", "Trying", "so", "Hard"};
+    
+    printf("This is my quote: ");
+    //for loop to iterate through 
+    int i;
+    for(i = 0; i < 4; i++)
+    {
+        if (sizeof(Array[i]) == '\0')
+        {
+            Array[i] = '_';
+        }
+       printf("\n%s\n\n", Array + i, Array + i, Array + i, Array + i);
+    }
+    printf("This is a null character: \n%c", Array[5]);
+return 0;
+    
+
+}
